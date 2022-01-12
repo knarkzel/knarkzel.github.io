f3128783c7e3a82e350618fef48e47b17cb75a8c
Knarkzel <knarkzel@gmail.com>
2022-01-12 22:37:33 +0100

    added minifier

--- build.sh
+++ build.sh
@@ -7,8 +7,13 @@ emacs -Q --script build-site.el
 cp content/styles.css public/styles.css
 cp content/favicon.ico public/favicon.ico
 
-# remove pre-wrap for source-code
+# remove pre-wrap for source-code and minify
 for f in $(find public/ -type f -name "*.html"); do
-    echo "Removing pre-wrap from $f"
     sed -i "s/white-space: pre-wrap;//g" $f
+    binaries/minify $f -o $f
+done
+
+# minify css
+for f in $(find public/ -type f -name "*.css"); do
+    binaries/minify $f -o $f
 done
