17b76b2acf8e342fff62abbd01b3c58f0ece7e77
Knarkzel <knarkzel@gmail.com>
2022-10-11 22:59:02 +0200

    Enable .gitignore support

--- build.sh
+++ build.sh
@@ -3,13 +3,9 @@
 # build site
 emacs -Q --script build.el
 
-# make directories
-for FILE in $(find src/ -type d); do
-    mkdir -p "public/${FILE:4}"
-done
-
-# copy static data
-for FILE in $(find src/ -type f -not -name "*.org"); do
+# copy static data and make directories
+for FILE in $(git ls-files src/ | grep -v .org); do
+    mkdir -p `dirname public/${FILE:4}`
     cp "src/${FILE:4}" "public/${FILE:4}"
 done
 
@@ -19,5 +15,3 @@ for FILE in $(find public/ -type f -name "*.html"); do
     [[ "$ABSOLUTE" == "/" ]] && EXTRA="" || EXTRA="/"
     sed -i "/Go back/! s|\.\/|$ABSOLUTE$EXTRA|g" "$FILE"
 done
-
-
